d6b3792116570f8e5cd95336a1e6273bcef833c9
Ryan Fleming <ryanfleming93@aol.com>
2020-12-11 00:59:48 +0000

    added apiRequest into key, need to verify pin with post request

--- react-atm/src/components/Key.js
+++ react-atm/src/components/Key.js
@@ -1,18 +1,48 @@
 import React from "react";
 import "./Key.css";
 import "./Display.css";
+import axios from "axios";
 
 function Key() {
   var keyClicked = [];
 
-  function checkValue(e) {
-    let pin = keyClicked.join("");
+  function checkValue() {
+    /* let pin = keyClicked.join("");
     if (pin === "1111") {
       console.log("Pin Entered Correctly");
     } else {
       console.log("Pin Entered Incorrectly");
+    } */
+    var pinCodeSeq = keyClicked.join("");
+
+    console.log(pinCodeSeq);
+    var pinFromApi = ApiRequester;
+    console.log(pinFromApi);
+
+    if (keyClicked === "1111" && keyClicked === ApiRequester) {
+      console.log("Pin is correct!");
+    } else {
+      console.log("Please enter the correct pin");
+    }
+
+    function ApiRequester() {
+      axios
+        .post(
+          "https://frontend-challenge.screencloud-michael.now.sh/api/pin/",
+          {
+            pin: "1111"
+            // checking if pin matches the pin in JSON
+          }
+        )
+        .then(function(response) {
+          let balance = response.data;
+          // returning balance from reponse
+          console.log(balance);
+          // logging balance
+        });
     }
   }
+
   /*
     keyClicked.join();
     if (keyClicked.value === "1111") {
