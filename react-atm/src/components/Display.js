ce5d62d585e2f539225a63c0ef9dbdb6f5c466b9
Ryan Fleming <ryanfleming93@aol.com>
2020-12-13 22:56:59 +0000

    updated styling, amended overdraft error issue

--- react-atm/src/components/Display.js
+++ react-atm/src/components/Display.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Display.css";
+//importing styles from display
 import "./Key.css";
+//importing styles from key
 import { useStateValue } from "../StateProvider";
+// StateProvider allows component to access current state of components
 
 function Display() {
   const [
     { balance, overdraft, logInError, loggedIn, input, withdrawing }
   ] = useStateValue();
+  // importing reducers from reducer.js
   return (
     <div className="display">
-      {!loggedIn && (
-        <p className="successMessage">Enter Pin: {input.map(char => "*")}</p>
-      )}
+      {!loggedIn && <p>Enter Pin: {input.map(char => "*")}</p>}
       {loggedIn && <p>Balance: {balance}</p>}
       {logInError && (
         <p className="errorMessage">
@@ -19,7 +21,7 @@ function Display() {
         </p>
       )}
       {withdrawing && <p>Enter Amount: {input} </p>}
-      {overdraft && (
+      {withdrawing && balance < 0 && (
         <p className="errorMessage">You have entered your overdraft</p>
       )}
     </div>
